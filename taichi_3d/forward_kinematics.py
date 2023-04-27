e381d892fbcce569e647d9613506d72fb245d31c
ShaimaaMonem <monem@mpi-magdeburg.mpg.de>
2023-04-27 21:25:35 +0200

    test

--- taichi_3d/forward_kinematics.py
+++ taichi_3d/forward_kinematics.py
@@ -3,58 +3,62 @@ import numpy as np
 
 ti.init(arch=ti.cpu)
 
-vecl = ti.Matrix.field(1, 100000, dtype=ti.f32, shape=())
-
 @ti.func
 def compute_rotation(alpha, beta, gamma):
-    R = ti.Vector([
-        ti.cos(alpha) * ti.cos(beta),
-        ti.sin(alpha) * ti.cos(beta),
-        -ti.sin(beta),
-        ti.cos(alpha) * ti.sin(beta) * ti.sin(gamma) - ti.sin(alpha) * ti.cos(gamma),
-        ti.sin(alpha) * ti.sin(beta) * ti.sin(gamma) + ti.cos(alpha) * ti.cos(gamma),
-        ti.cos(beta) * ti.sin(gamma),
-        ti.cos(alpha) * ti.sin(beta) * ti.cos(gamma) + ti.sin(alpha) * ti.sin(gamma),
-        ti.sin(alpha) * ti.sin(beta) * ti.cos(gamma) - ti.cos(alpha) * ti.sin(gamma),
-        ti.cos(beta) * ti.cos(gamma)
+    R = ti.Matrix([
+        [ti.cos(alpha) * ti.cos(beta), ti.sin(alpha) * ti.cos(beta), -ti.sin(beta)],
+        [ti.cos(alpha) * ti.sin(beta) * ti.sin(gamma) - ti.sin(alpha) * ti.cos(gamma),
+         ti.sin(alpha) * ti.sin(beta) * ti.sin(gamma) + ti.cos(alpha) * ti.cos(gamma),
+         ti.cos(beta) * ti.sin(gamma)],
+        [ti.cos(alpha) * ti.sin(beta) * ti.cos(gamma) + ti.sin(alpha) * ti.sin(gamma),
+         ti.sin(alpha) * ti.sin(beta) * ti.cos(gamma) - ti.cos(alpha) * ti.sin(gamma),
+         ti.cos(beta) * ti.cos(gamma)]
     ])
     return R
 
 @ti.kernel
 def forward_kinematics(handles: ti.ext_arr(), hier: ti.ext_arr(), eulers: ti.ext_arr(), T: ti.ext_arr(), new_handles: ti.ext_arr(), newR: ti.ext_arr()):
     n = handles.shape[0]
-    
-    for i in range(n):
-        lo = compute_rotation(eulers[i, 0], eulers[i, 1], eulers[i, 2])
-        
-        if hier[i, 0] == 0:
-            for j in range(T.shape[1]):
-                T[i, j] = lo[j]
-            for k in range(handles.shape[1]):
+    for i in range(n):    
+        lo = compute_rotation(eulers[i, 0], eulers[i, 1], eulers[i, 2]) # (3, 3)
+        if hier[i] == 0:  # what does this case mean?
+            for j in range(3):
+                for k in range(3):
+                    T[i, j * 3 + k] = ti.cast(lo[j, k], ti.f32) # F order
+            for k in range(3):
                 new_handles[i, k] = handles[i, k]
+                
         else:
-            for j in range(T.shape[1]):
-                newR[i - 1, j] = T[int(hier[i, 0] - 1), j]
             
-            Tp = ti.Matrix([[T[int(hier[i, 0] - 1), 0], T[int(hier[i, 0] - 1), 1], T[int(hier[i, 0] - 1), 2]],
-                            [T[int(hier[i, 0] - 1), 3], T[int(hier[i, 0] - 1), 4], T[int(hier[i, 0] - 1), 5]],
-                            [T[int(hier[i, 0] - 1), 6], T[int(hier[i, 0] - 1), 7], T[int(hier[i, 0] - 1), 8]]])
-            
-            
-            for l in range(handles.shape[1]):
-                vecl[0, l] = (handles[i, l] - handles[int(hier[i, 0] - 1), l])
-            for l in range(handles.shape[1]):
-                vec = new_handles[int(hier[i, 0] - 1), l] + Tp @ vecl
-                new_handles[i, l] = vec
+            for j in range(9):
+                newR[i - 1, j] = T[int(hier[i] - 1), j]  # only 
+                #print(newR[i - 1, j])
+                print("newR",newR[i - 1, j])
+            Tp = ti.Matrix([   # printing shows Tp is always the identity
+                [T[int(hier[i] - 1), 0], T[int(hier[i] - 1), 3], T[int(hier[i] - 1), 6]],
+                [T[int(hier[i] - 1), 1], T[int(hier[i] - 1), 4], T[int(hier[i] - 1), 7]],
+                [T[int(hier[i] - 1), 2], T[int(hier[i] - 1), 5], T[int(hier[i] - 1), 8]],
+            ])
+            print("Tp", Tp)
+            vecl = ti.Vector([handles[i, l] - handles[int(hier[i] - 1), l] for l in range(3)]) #(3,)
+            vec = ti.Vector([0.0, 0.0, 0.0])
+            for l in range(3):
+                vec[l] = ti.cast(new_handles[int(hier[i] - 1), l] + Tp[l, :] @ vecl, ti.f32) #(3,)
+            print("vec",vec)
+            for l in range(3):
+                new_handles[i, l] = vec[l]
+                print(new_handles[i, l])
+            print("lo", lo)
+            temp = Tp @ lo
+            print("temp", temp)
+            for j in range(3):
+                for k in range(3):
+                    T[i, j * 3 + k] = ti.cast(temp[j, k], ti.f32)
+                    print("T", T[i, j * 3 + k] )
 
-            lo_matrix = ti.Matrix([[lo[0], lo[1], lo[2]], [lo[3], lo[4], lo[5]], [lo[6], lo[7], lo[8]]])
-            temp = Tp @ lo_matrix
-            for j in range(T.shape[1]):
-                T[i, j] = temp[j]
 
 
-
-
+"""
 # Example input data
 handles = np.array([
     [0.0, 0.0, 0.0],  # Root joint
@@ -79,13 +83,17 @@ T = np.tile(np.array([1, 0, 0, 0, 1, 0, 0, 0, 1]), (n, 1))
 # Output arrays
 
 new_handles = np.zeros_like(handles)
-newR = np.zeros((handles.shape[0] - 1, 9))
+# Create the newR array outside the kernel
+n_newR = handles.shape[0] - 1
+newR = np.tile(np.array([1, 0, 0, 0, 1, 0, 0, 0, 1]), (n_newR, 1))
 
 
 # Run the forward_kinematics function
 forward_kinematics(handles, hierarchy, eulers, T, new_handles, newR)
 
 print("New handles:")
-print(new_handles)
+print(new_handles.shape)
 print("\nNew rotations:")
-print(newR)
+print(newR.shape)
+
+"""
