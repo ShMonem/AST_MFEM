f3984be7ea4d4d2f69313d7e982120aa4ad0065c
ShaimaaMonem <monem@mpi-magdeburg.mpg.de>
2023-05-03 19:05:42 +0200

    upload/update more functions

--- taichi_3d/GS.py
+++ taichi_3d/GS.py
@@ -1,61 +1,79 @@
 import taichi as ti
 import numpy as np
 
+
 ti.init(arch=ti.cpu)
 
 @ti.kernel
-def gauss_seidel(L: ti.template(), U: ti.template(), b: ti.template(), x: ti.template(), n: int):
+def gauss_seidel(L: ti.template(), U: ti.template(), b: ti.template(), x: ti.template(),\
+                 itr: ti.i32, Udim1: ti.i32, Udim2 : ti.i32, x_old: ti.template(), \
+                 result: ti.template(), solver: ti.template()):
     
-    for iter in range(n):
-        for i in range(x.shape[0]):
-            sum = ti.cast(0.0, ti.f32)
-            for j in range(x.shape[0]):
-                if i != j:
-                    sum += L[i, j] * x[j]
-            x[i] = (b[i] - sum) / L[i, i]
+
+    for iter in range(itr):
+        for i in range(Udim2):
+            x_old[i] = x[i]
+
+        for i in range(Udim1):
+            res = 0.0
+            for j in range(Udim2):
+                res += U[i, j] * x[j]
+            result[i] = b[i] - res
+
+        x = solver.solve(result)
+
 
 def gauss_seidel_solver(A, b, n, sol):
-    # create an ndarray fron the tichi field A:
-    A_np = np.zeros((A.shape[0], A.shape[1]), dtype=np.float32)
+    A_field = ti.field(dtype=ti.f32, shape=A.shape)
     for i in range(A.shape[0]):
         for j in range(A.shape[1]):
-            A_np[i, j] = A[i, j]
+            A_field[i, j] = A[i, j]
 
-    L_np = np.tril(A_np)
-    U_np = A_np - L_np
+    L_np = np.tril(A)
+    U_np = A - L_np
     
-    L = ti.field(dtype=ti.f32, shape=(A.shape[0], A.shape[1]))
-    U = ti.field(dtype=ti.f32, shape=(A.shape[0], A.shape[1]))
-
-    @ti.kernel
-    def initialize_L_and_U(L: ti.template(), U: ti.template(), L_np: ti.ext_arr(), U_np: ti.ext_arr()):
-        for i, j in L:
+    L = ti.field(dtype=ti.f32, shape=L_np.shape)
+    U = ti.field(dtype=ti.f32, shape=U_np.shape)
+    
+    for i in range(L_np.shape[0]):
+        for j in range(L_np.shape[1]):
             L[i, j] = L_np[i, j]
             U[i, j] = U_np[i, j]
 
-    initialize_L_and_U(L, U, L_np, U_np)
-
     x = ti.field(dtype=ti.f32, shape=(sol.shape[0],))
+    
     for i in range(sol.shape[0]):
         x[i] = sol[i]
 
     b_field = ti.field(dtype=ti.f32, shape=(b.shape[0],))
     for i in range(b.shape[0]):
         b_field[i] = b[i]
+    
+    result = ti.field(dtype=ti.f32, shape=U_np.shape[0])
+    x_old = ti.field(dtype=ti.f32, shape=(x.shape[0],))
 
-    gauss_seidel(L, U, b_field, x, n)
+    for i in range(x.shape[0]):
+        x_old[i] = x[i]
+
+    print(L_np.shape)
+    ti_field = ti.field(dtype=ti.f32, shape=L_np.shape)
+    ti_field.from_numpy(L_np)
+    solver = ti.linalg.SparseSolver()
+    solver.compute(ti_field)
+
+    gauss_seidel(L, U, b_field, x, itr, U_np.shape[0], U_np.shape[1], x_old, result, solver)
 
     x_np = np.array([x[i] for i in range(sol.shape[0])])
     return x_np
 
 
-"""
+
+
+
 A = np.array([[4, -1, 0, 0], [-1, 4, -1, 0], [0, -1, 4, -1], [0, 0, -1, 3]], dtype=np.float32)
 b = np.array([15, 10, 10, 10], dtype=np.float32)
 sol = np.array([0, 0, 0, 0], dtype=np.float32)
-n = 25
+itr = 25
 
-x = gauss_seidel_solver(A, b, n, sol)
+x = gauss_seidel_solver(A, b, itr, sol)
 print(x)
-"""
-
