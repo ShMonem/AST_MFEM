3635f4ded840f2c7efb25b421e581576a71f561b
ShaimaaMonem <monem@mpi-magdeburg.mpg.de>
2023-05-05 00:12:05 +0200

    GS solver fixed

--- taichi_3d/GS.py
+++ taichi_3d/GS.py
@@ -4,89 +4,109 @@ from scipy import sparse
 
 ti.init(arch=ti.cpu)
 
-#@ti.kernel
-def gauss_seidel(b: ti.template(), x: ti.template(),\
-                 itr: ti.i32, xdim: ti.i32, x_old: ti.template(), \
-                 result: ti.template(), solver: ti.template()):
+@ti.kernel
+def gauss_seidel_itr(U:  ti.template(), b: ti.template(), x: ti.template(),\
+                 x_old: ti.template(), result: ti.template()):
+    Udim1 = U.shape[0]
+    Udim2 = U.shape[1]  
+    xdim = x.shape[0]      
+    for i in range(xdim):
+        x_old[i] = x[i]
     
+    for i in range(Udim1):
+        res = 0.0
+        for j in range(Udim2):
+            res += U[i, j] * x[j]
+        result[i] = b[i] - res
 
+## python function
+def gauss_seidel(A:  ti.ext_arr(), b: ti.template(), x: ti.template(),\
+                 itr: ti.i32, x_old: ti.template(), \
+                 result: ti.template()):
+    
+    U, solver = A_L_sum_U(A)
+    
+    x_itr = ti.field(dtype=ti.f32, shape=(x.shape[0],))
+    #fill_field(x_itr, x)?
     for iter in range(itr):
-        for i in range(xdim):
-            x_old[i] = x[i]
-        """
-        for i in range(Udim1):
-            res = 0.0
-            for j in range(Udim2):
-                res += U[i, j] * x[j]
-            result[i] = b[i] - res
-        """
-        x = solver.solve(result) ## cannot be called in a taichi function: return self.solver.solve(b.to_numpy())
+        print(iter)
+        gauss_seidel_itr(U, b, x_itr, x_old, result)
+        xtemp = solver.solve(result) ## cannot be called in a taichi function: return self.solver.solve(b.to_numpy())
+        fill_field(x_itr, xtemp)  ## xtemp is a numpy array. After each itr, we convert it to ti.template
+                              ## so that it be used by the ti.kernel gauss_seidel_itr
+    return x_itr
 
-
-def gauss_seidel_solver(A, b, n, sol):
-    A_field = ti.field(dtype=ti.f32, shape=A.shape)
-    A_field.from_numpy(A)
-
-    tribletsNum = sparse.csr_matrix(A).nnz
-
-    A_sparse_ti = ti.linalg.SparseMatrix(n=A.shape[0], m=A.shape[1], dtype=ti.f32) 
-    triplets = ti.Vector.ndarray(n=3, dtype=ti.f32, shape=A.shape[0])
-
-    def fill_triplets(triplets: ti.types.ndarray(), A: ti.ext_arr()):
-        for i in range(A.shape[0]):
-            for j in range(A.shape[1]):
-                triplets[i] = ti.Vector([i, j, A[i,j]], dt=ti.f32)
+@ti.kernel
+def fill_U(U: ti.template(), U_np: ti.ext_arr()):
     
-    fill_triplets(triplets, A)
-
-    A_sparse_ti.build_from_ndarray(triplets)
-    """
-    L_np = np.tril(A)
-    U_np = A - L_np
-    
-    L = ti.field(dtype=ti.f32, shape=L_np.shape)
-    U = ti.field(dtype=ti.f32, shape=U_np.shape)
-    
-    for i in range(L_np.shape[0]):
-        for j in range(L_np.shape[1]):
-            L[i, j] = L_np[i, j]
+    Udim1 = U_np.shape[0]
+    Udim2 = U_np.shape[1]
+    for i in range(Udim1):
+        for j in range(Udim2):
+            """
+            try:
+                U_np[i, j]
+                U[i, j] = U_np[i, j]
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                print(i, j)
+            """
             U[i, j] = U_np[i, j]
-
-    print(L_np.shape)
-    ti_field = ti.field(dtype=ti.f32, shape=L_np.shape)
-    ti_field.from_numpy(L_np)
-    """
-    x = ti.field(dtype=ti.f32, shape=(sol.shape[0],))
     
+@ti.kernel
+def fill_triplets(triplets: ti.types.ndarray(), A: ti.ext_arr()):
+    for i in range(A.shape[0]):
+        for j in range(A.shape[1]):
+            triplets[i*A.shape[1]+ j] = ti.Vector([i, j, A[i,j]], dt=ti.f32)
+            #print(triplets[i])
+@ti.kernel
+def fill_field(x: ti.template(),  sol: ti.ext_arr()):
+
     for i in range(sol.shape[0]):
         x[i] = sol[i]
 
-    b_field = ti.field(dtype=ti.f32, shape=(b.shape[0],))
-    for i in range(b.shape[0]):
-        b_field[i] = b[i]
+def A_L_sum_U(A):  ## TODO: ti.kernel
+    L_np = np.tril(A)
+    U_np = A - L_np
     
-    result = ti.field(dtype=ti.f32, shape=x.shape[0])
-    x_old = ti.field(dtype=ti.f32, shape=(x.shape[0],))
-
-    for i in range(x.shape[0]):
-        x_old[i] = x[i]
-
+    U = ti.field(dtype=ti.f32, shape=U_np.shape)
+    fill_U(U, U_np)
+    
+    L = ti.linalg.SparseMatrix(n=L_np.shape[0], m=L_np.shape[1], dtype=ti.f32) 
+    triplets = ti.Vector.ndarray(n=3, dtype=ti.f32, shape=L_np.shape[0]*2)
+    fill_triplets(triplets, L_np)
+    L.build_from_ndarray(triplets)  ## not filled correctly
 
     solver = ti.linalg.SparseSolver()
-    solver.compute(A_sparse_ti)
-
-    gauss_seidel(b_field, x, itr, x.shape[0], x_old, result, solver)
-
-    x_np = np.array([x[i] for i in range(sol.shape[0])])
-    return x_np
-
+    solver.compute(L)
+    return U, solver
 
 """
+## numerical example
 A = np.array([[4, -1, 0, 0], [-1, 4, -1, 0], [0, -1, 4, -1], [0, 0, -1, 3]], dtype=np.float32)
 b = np.array([15, 10, 10, 10], dtype=np.float32)
 sol = np.array([0, 0, 0, 0], dtype=np.float32)
 itr = 25
 
-x = gauss_seidel_solver(A, b, itr, sol)
+#U, solver = A_L_sum_U(A)
+x = ti.field(dtype=ti.f32, shape=(sol.shape[0],))
+b_field = ti.field(dtype=ti.f32, shape=(b.shape[0],))
+
+x_old = ti.field(dtype=ti.f32, shape=(x.shape[0],))
+result = ti.field(dtype=ti.f32, shape=x.shape[0])
+
+fill_field(x, sol)
+fill_field(x_old, sol)
+fill_field(b_field, b)
+
+x = gauss_seidel(A, b_field, x, itr, x_old, result)
+
+x_np = np.array([x[i] for i in range(sol.shape[0])])
+
 print(x)
 """
+
+
+
+
+
--- taichi_3d/V_Cycle.py
+++ taichi_3d/V_Cycle.py
@@ -1,71 +1,122 @@
 import taichi as ti
 import numpy as np
 
+# gauss_seidel_solver is a python function, and is defined in GS
 from GS import *
 
-ti.init(arch=ti.cpu)
+ti.init()
 
-def V_Cycle(A, b, n, sol, level):
-    sol = gauss_seidel_solver(A, b, n, sol)
 
-    r = b - A @ sol
-    rhs = U[level].transpose() @ r
-    e = ti.Vector.zero(ti.f32, n)
+placed_fields = False
 
-    if level == l - 1:
-        e = ti.Matrix.solve(UTAU[level], rhs)
+@ti.kernel
+def compute_residual(A: ti.template(), b: ti.template(), x: ti.template(), r: ti.template()):
+    for i in range(A.shape[0]):
+        sum = ti.cast(0.0, ti.f32)
+        for j in range(A.shape[1]):
+            sum += A[i, j] * x[j]
+        r[i] = b[i] - sum
+
+@ti.kernel
+def update_sol_with_e(sol: ti.template(), U: ti.template(), e: ti.ext_arr()):
+    for i in range(sol.shape[0]):
+        sum = ti.cast(0.0, ti.f32)
+        for j in range(U.shape[1]):
+            sum += U[i, j] * e[j]
+        sol[i] += sum
+
+def restrict(Ub_l: ti.template(), res: ti.template(), red_res: ti.template()):
+    ## Ub_l: class 'taichi.linalg.sparse_matrix.SparseMatrix'
+    # project error to lower resolution grid
+    red_res = Ub_l.transpose() @ res
+
+def atom_add(x: ti.template(), y:ti.template()):
+    ti.atomic_add(x, y)
+def v_cycle(A_ti: ti.template(), b: ti.template(), UTAU, Ub, l, \
+            A:  ti.template(), x: ti.template(),\
+                 itr: ti.i32, x_old: ti.template(), \
+                 result: ti.template()):
+    
+    sol = gauss_seidel(A, b, x, itr, x_old, result)
+    
+    r = ti.field(dtype=ti.f32, shape=(A.shape[0],))
+    compute_residual(A_ti, b, sol, r)
+
+    red_res= ti.field(dtype=ti.f32, shape=(Ub[l].shape[0],))
+    restrict(Ub[l], r, red_res)
+
+    e = np.zeros(red_res.shape)
+
+    if l == (len(Ub) -1 ): # reached the last reduction matrix in the list
+        e = basis_solvers[l].solve(red_res)
     else:
-        e = V_Cycle(UTAU[level], rhs, n, e, level + 1)
+        e = v_cycle(A_ti, b, UTAU, Ub, l+1, \
+              A, x, itr, x_old, result, UTAU_shapes)
+      
+    atom_add(sol, Ub[l]@ e)  ## TODO: has "sol" been updated?
 
-    sol = sol + U[level] @ e
-    if level == 0:
-        sol = gauss_seidel_solver(A, b, n, sol)
+
+    if l == 0:
+        sol = gauss_seidel(A, b, sol, itr, x_old, result)
     else:
-        sol = gauss_seidel_solver(UTAU[level - 1], b, n, sol)
-
+        sol = gauss_seidel(UTAU[l], b, sol, itr, x_old, result)
     return sol
 
 
-@ti.kernel
-def v_cycle_kernel(A: ti.template(), b: ti.template(), n: int, init: ti.template(), l: int):
-    sol[None] = V_Cycle(A, b, n, init, l)
+"""
+# Example usage:
+A = np.array([[10, -1, 0, 0], [-1, 4, -18, 0], [5, -1, 42, -1], [0, 0, -1, 3]], dtype=np.float32)
+b = np.array([15, 10, 10, 10], dtype=np.float32)
+sol_init = np.array([0, 0, 0, 0], dtype=np.float32)
+n = 25
+U1t = np.eye(b.shape[0], dtype=np.float32)
+Ut = []  # list of numpy reduction matrices
+Ut.append(U1t) 
+
+Ub = [] # list of taichi reduction matrices
+Ub_shapes = []
+for l in range(len(Ut)):
+    U_l = ti.linalg.SparseMatrix(n=Ut[l].shape[0], m=Ut[l].shape[1], dtype=ti.f32) 
+    triplets_u_l = ti.Vector.ndarray(n=3, dtype=ti.f32, shape= Ut[l].shape[0])
+    fill_triplets(triplets_u_l, Ut[l])
+    U_l.build_from_ndarray(triplets_u_l) ## TODO: not filled properly
+    Ub.append(U_l)  # basis list
+    Ub_shapes.append(Ut[l].shape)
+
+UTAU = []
+UTAU_shapes =[]
+for l in range(len(Ut)):
+    UTAUt_l = Ut[l].T @ A @ Ut[l] 
+    UTAU_l = ti.linalg.SparseMatrix(n=UTAUt_l.shape[0], m=UTAUt_l.shape[1], dtype=ti.f32)
+    triplets_utau_l = ti.Vector.ndarray(n=3, dtype=ti.f32, shape= UTAUt_l.shape[0])
+    fill_triplets(triplets_utau_l, UTAUt_l)
+    UTAU_l.build_from_ndarray(triplets_utau_l) ## TODO: not filled properly
+    UTAU.append(UTAU_l) # projected basis list
+    UTAU_shapes.append(UTAUt_l.shape)
+
+basis_solvers = [] ## list of UTAU solvers
+for l in range(len(UTAU)):
+    solver_l = ti.linalg.SparseSolver()
+    solver_l.compute(UTAU[l])
+    basis_solvers.append(solver_l)
+
+A_field = ti.field(dtype=ti.f32, shape=(A.shape[0], A.shape[1]))
+b_field = ti.field(dtype=ti.f32, shape=(b.shape[0],))
+
+fill_U(A_field, A)
+fill_field(b_field, b)
+
+sol_init = np.zeros(b.shape, dtype=np.float32)
+x = ti.field(dtype=ti.f32, shape=(sol_init.shape[0],))
+x_old = ti.field(dtype=ti.f32, shape=(x.shape[0],))
+result = ti.field(dtype=ti.f32, shape=x.shape[0])
+fill_field(x, sol_init)
 
 
+l =0
+itr = 24
+sol = v_cycle(A_field, b_field, UTAU, Ub, l, \
+              A, x, itr, x_old, result)
+print(sol)
 
-if __name__ == "__main__":
-
-    # Set dimensions for matrices and vectors
-    n = 64
-    l = 3
-
-    # Create Taichi fields
-    A = ti.field(ti.f32, (n, n), needs_grad=False)
-    b = ti.field(ti.f32, (n,), needs_grad=False)
-    UTAU = [ti.field(ti.f32, (n, n), needs_grad=False) for _ in range(l)]
-    U = [ti.field(ti.f32, (n, n), needs_grad=False) for _ in range(l)]
-    init = ti.Vector.field(n, dtype=ti.f32, shape=(), needs_grad=False)
-    sol = ti.Vector.field(n, dtype=ti.f32, shape=(), needs_grad=False)
-
-    # Fill A, b, UTAU, and U with example data
-    A_np = np.random.rand(n, n).astype(np.float32)
-    b_np = np.random.rand(n).astype(np.float32)
-    UTAU_np = [np.random.rand(n, n).astype(np.float32) for _ in range(l)]
-    U_np = [np.random.rand(n, n).astype(np.float32) for _ in range(l)]
-    init_np = np.random.rand(n).astype(np.float32)
-    sol_np = np.zeros(n, dtype=np.float32)
-
-    A.from_numpy(A_np)
-    b.from_numpy(b_np)
-    for i in range(l):
-        UTAU[i].from_numpy(UTAU_np[i])
-        U[i].from_numpy(U_np[i])
-    init.from_numpy(init_np)
-
-    # Validate the length of the list U
-    if len(U) != l:
-        raise ValueError(f"The length of the list U ({len(U)}) must match the parameter l ({l}).")
-
-    v_cycle_kernel(A, b, n, init, len(U))
-
-    sol_np = sol[None].to_numpy()
-    print(sol_np)
+"""
