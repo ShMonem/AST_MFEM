07e0d0e5ff8f7502fa2c43fa6491b3b261013537
ShaimaaMonem <monem@mpi-magdeburg.mpg.de>
2023-05-04 12:28:11 +0200

    compute_J updated

--- taichi_3d/compute_J.py
+++ taichi_3d/compute_J.py
@@ -11,7 +11,8 @@ ti.init(arch=ti.cpu)   # Initialize Taichi
 def compute_J(R_mat: ti.types.ndarray(), B: ti.types.ndarray()) -> ti.types.ndarray():
     J = lil_matrix((R_mat.shape[1], B.shape[1]))
     for k in range(B.shape[1]):
-        J[:, k], istop, itn, r1norm = lsqr(R_mat, B[:,k].todense(), iter_lim =4)[:4]
+        b = x = np.squeeze(np.asarray(B[:,k].todense()))
+        J[:, k], istop, itn, r1norm = lsqr(R_mat, b, iter_lim =4)[:4]
 
         #print( "sol: ", J[:, k], "termination reason: ", istop, "itr num: ", itn, "error: ", r1norm)
     return J
